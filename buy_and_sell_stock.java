78e6475fca68fbb98632eae74af2430d7d767421
kgole <kartikgole77@gmail.com>
2025-02-23 17:43:10 -0600

    daily temperatures

--- buy_and_sell_stock.java
+++ buy_and_sell_stock.java
@@ -1,6 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
+        // brute force
+        // int result = 0;
+
+        // for(int i=0; i<prices.length-1; i++){
+        //     for(int j=i+1; j<prices.length; j++){
+        //         if(prices[i] < prices[j]){
+        //             result = Math.max(result, prices[j] - prices[i]);
+        //         }
+        //     }
+        // }
+        
+        // return result;
+
         int result = 0;
         int min = Integer.MAX_VALUE;
         for(int i=0; i<prices.length;i++){
