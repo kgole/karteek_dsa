487b925f2856bcc7f374a4c5fa51c3206989881e
kgole <kartikgole77@gmail.com>
2025-03-09 14:30:11 -0500

    iterative dfs max depth of binary tree

--- tree/max_depth_of_binary_tree.java
+++ tree/max_depth_of_binary_tree.java
@@ -16,15 +16,38 @@
 class Solution {
     public int maxDepth(TreeNode root) {
 
-        if(root == null){
+        // if(root == null){
+        //     return 0;
+        // }
+
+        // int left_height = maxDepth(root.left);
+        // int right_height = maxDepth(root.right);
+
+        // return Math.max(left_height, right_height) + 1;
+
+        
+        
+        if (root == null) {
             return 0;
         }
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
 
-        return Math.max(left+1, right+1);
-        
-        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size(); 
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 }
